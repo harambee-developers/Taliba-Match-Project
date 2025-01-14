81559512d1d8f797b7bb6cdff59320fbd1eada43
Raufkhw <rraufrahim@outlook.com>
2025-01-14 23:37:06 +0000

    Mobile Logo Centring

--- client/src/components/LandingNavbar.jsx
+++ client/src/components/LandingNavbar.jsx
@@ -6,15 +6,15 @@ const LandingNavbar = () => {
     return (
         <div className="relative w-full bg-[#FFF1FE] lg:flex">
             {/* Left Section */}
-            <div className="flex items-center w-full lg:w-1/2 px-8 py-4 z-20">
+            <div className="flex flex-col items-center lg:flex-row lg:items-center w-full lg:w-1/2 lg:px-8 py-4 z-20">
                 <Link to="/">
                     <img
                         src={talibahLogo}
                         alt="Talibah Match Logo"
-                        className="w-12 h-12"
+                        className="w-12 h-12 mx-auto lg:mx-0"
                     />
                 </Link>
-                <h1 className="text-2xl font-bold text-[#800020] ml-4 font-[Montserrat]">
+                <h1 className="text-2xl font-bold text-[#800020] mt-2 lg:mt-0 lg:ml-4 font-[Montserrat] text-center lg:text-left">
                     Talibah Match
                 </h1>
             </div>
