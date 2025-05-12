f32e50444ff475be932614cbf3ca3ba48a0afb92
hmohammed2 <hamza_mohammed15@hotmail.com>
2025-05-12 19:20:58 +0100

    fixed landing page removed match keyword

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,33 +1,19 @@
 import React from "react";
-import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Subscription from "./pages/Subscription";
 import LandingPage from "./pages/LandingPage";
 import Register from "./pages/Register";
 import Navbar from "./components/Navbar";
-import LandingNavbar from "./components/LandingNavbar"; // Import LandingNavbar
 import AdminDashboard from "./pages/AdminDashboard";
 import AdminLogin from "./pages/AdminLogin";
 import RegisterSuccess from "./pages/RegisterSuccess";
 
 function App() {
   // Custom component to switch navbars based on the route
-  const Layout = ({ children }) => {
-    const location = useLocation();
-
-    // Conditional rendering of Navbar
-    const isLandingPage = location.pathname === "/";
-
-    return (
-      <>
-        {isLandingPage ? <LandingNavbar /> : <Navbar />}
-        {children}
-      </>
-    );
-  };
 
   return (
     <BrowserRouter>
-      <Layout>
+      <Navbar />
         <Routes>
           <Route path="/" element={<LandingPage />} /> {/* Landing Page */}
           <Route path="/register" element={<Register />} /> {/* Register */}
@@ -36,7 +22,6 @@ function App() {
           <Route path="/admin" element={<AdminLogin />} />{" "}
           <Route path="/admin/dashboard" element={<AdminDashboard />} />{" "}
         </Routes>
-      </Layout>
     </BrowserRouter>
   );
 }
