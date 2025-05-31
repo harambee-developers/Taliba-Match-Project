abece55561d511aed7022e3494709d8c300b88e7
hmohammed2 <hamza_mohammed15@hotmail.com>
2025-05-31 19:49:58 +0100

    Removed merge conflicts

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,29 +1,4 @@
 import React from "react";
-<<<<<<< HEAD
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Subscription from "./pages/Subscription";
-import LandingPage from "./pages/LandingPage";
-import Register from "./pages/Register";
-import Navbar from "./components/Navbar";
-import AdminDashboard from "./pages/AdminDashboard";
-import AdminLogin from "./pages/AdminLogin";
-import RegisterSuccess from "./pages/RegisterSuccess";
-
-function App() {
-  // Custom component to switch navbars based on the route
-
-  return (
-    <BrowserRouter>
-      <Navbar />
-        <Routes>
-          <Route path="/" element={<LandingPage />} /> {/* Landing Page */}
-          <Route path="/register" element={<Register />} /> {/* Register */}
-          <Route path="/subscribe" element={<Subscription />} /> {/* Subscription */}
-          <Route path="/register-success" element={<RegisterSuccess />} /> {/* Subscription */}
-          <Route path="/admin" element={<AdminLogin />} />{" "}
-          <Route path="/admin/dashboard" element={<AdminDashboard />} />{" "}
-        </Routes>
-=======
 import { BrowserRouter} from "react-router-dom";
 import AppLayout from "./AppLayout";
 
@@ -32,7 +7,6 @@ function App() {
   return (
     <BrowserRouter>
       <AppLayout/>
->>>>>>> UAT
     </BrowserRouter>
   );
 }
